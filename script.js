5ce2fabc5ee44f48b003e8b03d541856a85ac627
gary2504 <garychaves25@gmail.com>
2024-04-30 15:44:46 -0600

    cambiar color mediante botón y cambiar color de borde

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+let boton = document.getElementById("cambio")
+boton.addEventListener('click', () => {
+    
+    if (document.getElementById("borde2").style.color == "lightblue" && document.getElementById("borde2").style.backgroundColor == "slategray") {
+        document.getElementById("borde2").style.backgroundColor = "lightblue"
+        document.getElementById("borde2").style.color = "slategray"
+        document.getElementById("borde").style.border = "1px solid black" 
+        
+    
+    }else{
+        document.getElementById("borde2").style.backgroundColor = "slategray"
+        document.getElementById("borde2").style.color = "lightblue"
+        document.getElementById("borde").style.border = " 1px solid white"
+         
+    }
+});
+
 
 
 
